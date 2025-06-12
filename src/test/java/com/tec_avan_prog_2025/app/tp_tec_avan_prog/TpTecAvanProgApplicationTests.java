bd66a49a65b03124613c15f930a1d474df9a2849
Eloy Romero <eloyromero2003@gmail.com>
2025-06-11 21:52:37 -0300

    Arreglo de test

--- src/test/java/com/tec_avan_prog_2025/app/tp_tec_avan_prog/TpTecAvanProgApplicationTests.java
+++ src/test/java/com/tec_avan_prog_2025/app/tp_tec_avan_prog/TpTecAvanProgApplicationTests.java
@@ -2,8 +2,10 @@ package com.tec_avan_prog_2025.app.tp_tec_avan_prog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TpTecAvanProgApplicationTests {
 
 	@Test
