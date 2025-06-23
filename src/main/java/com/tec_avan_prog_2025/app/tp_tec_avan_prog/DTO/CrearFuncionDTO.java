bee65097dda5e233dcb5c9d7bd6f870b884048c3
EloyRomero81 <eloyromero2003@gmail.com>
2025-06-23 17:31:09 -0300

    Renombrado

---
+++ src/main/java/com/tec_avan_prog_2025/app/tp_tec_avan_prog/DTO/CrearFuncionDTO.java
@@ -0,0 +1,25 @@
+package com.tec_avan_prog_2025.app.tp_tec_avan_prog.DTO;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CrearFuncionDTO {
+    private Integer idCuenta;
+    private Integer idFuncion;
+    private Integer nroSala;
+    private String nombreArtista;
+    private LocalDate fecha;
+    private LocalTime hora; 
+    private Integer duracion;
+    private String tipoFuncion;
+    private Double precioBaseEntrada; 
+}
